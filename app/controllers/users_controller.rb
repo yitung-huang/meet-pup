c2535876cc9c510bdd95a1095a549f450fd05ebc
Thomas <yitunghuang83@gmail.com>
2020-03-24 13:04:46 +1030

    Add User Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'landing/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users
+
   root 'landing#index'
 
 end
