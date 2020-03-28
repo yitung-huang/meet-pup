0de5628e647d784325ec1673c5bf53b431e4d0fb
Thomas <yitunghuang83@gmail.com>
2020-03-28 18:32:25 +1030

    Add create action and view for Dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @dog = Dog.new
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @dog = @user.dogs.create(dog_params)
+
+    redirect_to user_path(@user)
+  end
+
+  private
+    def dog_params
+      params.require(:dog).permit(:name, :age, :gender, :size)
+    end
+
 end
